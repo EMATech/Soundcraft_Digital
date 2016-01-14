2b75b961fc64781401c82c81c9242f7d3f4fa34f
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2016-01-14 15:21:50 +0100

    Added missing commands

--- firewire-request/script1/script1.sh
+++ firewire-request/script1/script1.sh
@@ -3,13 +3,11 @@
 # External input plug of the unut
 # Maybe, fails for plug 0 and plug 18
 for i in $(seq 0 18); do
-     echo "----Input plug "$i"----"
      firewire-request /dev/fw1 fcp $(printf '01ff02c0000001%02xff01ffff\n' $i)
 done
 
 # External output plug of the unit
 # Maybe, fails for plug 0 and plug 16
 for i in $(seq 0 16); do
-     echo "----Output plug "$i"----"
      firewire-request /dev/fw1 fcp $(printf '01ff02c0100001%02xff01ffff\n' $i)
 done
